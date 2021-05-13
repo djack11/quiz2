07fc9aae5a8f03cd4a963f9ed08f359933bc88e4
djack11 <djack11@hotmail.com>
2021-05-12 20:45:09 -0400

    1stattempt

---
+++ quiz2package/Main.java
@@ -0,0 +1,47 @@
+package quiz2package;
+
+
+
+
+import java.util.Scanner;
+
+
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner grade = new Scanner(System.in);
+
+    System.out.println("Hello! Please enter a grade:");
+
+   
+    int value1 = grade();
+    
+    int grade1 = grade();
+    
+    
+
+    // Output input by user
+    System.out.println("grade: " + grade1);
+    
+    System.out.println("Would you like to enter another grade"(Y/N)");"
+    
+    		
+    		
+    		System.out.println(highestGrade + " get highest grade);
+            System.out.println(lowesGrade + " get lowest grade);"
+            System.out.println(averageGrade + " get average grade);
+            
+}
+
+
+}
+
+
+
+
+
+
+
+
+}
---
+++ quiz2package/grade.java
@@ -0,0 +1,22 @@
+package quiz2package;
+
+public class grade {
+	
+	public static void main(String[] args) {
+	}
+		
+		private int grade;
+		
+		private grade() {
+			
+		
+				
+				
+				
+				
+			}
+		}
+				
+				
+			
+  
